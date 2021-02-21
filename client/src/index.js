8688338968a6ddb27c278ce75884230d205dd8e1
José Guerreiro <jose-guerreiro@live.com.pt>
2021-02-21 13:39:09 +0000

    Added abortcontroller-polyfill to support older iOS versions

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import 'abortcontroller-polyfill'
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom'
 //import registerServiceWorker from './utils/registerServiceWorker'
