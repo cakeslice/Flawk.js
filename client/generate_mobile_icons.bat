bce6016a1e2708eeacc43cd017eaf25deb142e36
José Guerreiro <jose-guerreiro@live.com.pt>
2021-03-12 13:15:07 +0000

    Removed redundant warning

--- client/generate_mobile_icons.bat
+++ client/generate_mobile_icons.bat
@@ -2,7 +2,6 @@
 setlocal
 
 cordova-res ios --skip-config --copy & cordova-res android --skip-config --copy
-REM Copy all icons in android/app/src/main/res/mipmap* to replace ic_launcher_foreground.png
 
 :END
 endlocal
