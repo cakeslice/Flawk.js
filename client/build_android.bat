95569a850633f942eba8a22639e845d87733b0b7
José Guerreiro <jose-guerreiro@live.com.pt>
2021-05-19 19:40:14 +0100

    Fixed android build script

--- client/build_android.bat
+++ client/build_android.bat
@@ -5,7 +5,9 @@ set CI=false
 set SKIP_PREFLIGHT_CHECK=true
 set GENERATE_SOURCEMAP=true
 
-npm run buildLocal & npx cap sync & cd android & gradlew assembleRelease & cd app/build/outputs/apk/release & jarsigner -keystore YOUR_KEYSTORE_PATH -storepass YOUR_KEYSTORE_PASS app-release-unsigned.apk YOUR_KEYSTORE_ALIAS & zipalign 4 app-release-unsigned.apk app-release.apk & cd ../../../../../
+npm run buildLocal & npx cap sync & cd android & gradlew bundleRelease & cd app/build/outputs/apk/release & jarsigner -keystore ./../../../../../../.keystore -storepass YOUR_KEYSTORE_PASS app-release.aab YOUR_KEYSTORE_ALIAS & cd ../../../../../../
+
+REM keytool -genkey -v -keystore .keystore -alias YOUR_KEYSTORE_ALIAS -keyalg RSA -keysize 2048 -validity 10000
 
 :END
 endlocal
