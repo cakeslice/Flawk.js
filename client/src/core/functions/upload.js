274163af7164c04e9cc6db90f0d6acf1043a8631
José Guerreiro <jose-guerreiro@live.com.pt>
2021-11-01 17:39:34 +0000

    Copyright notices

--- client/src/core/functions/upload.js
+++ client/src/core/functions/upload.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020 José Guerreiro. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const config = require('../config_').default
 
 //
--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020 José Guerreiro. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
