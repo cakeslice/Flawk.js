0c64d3df0d060f888b408266e5d3cda285780e6c
José Guerreiro <jose-guerreiro@live.com.pt>
2022-03-02 17:42:38 +0000

    Ignore eslint error for Microsoft Clarity script

--- client/src/core/components/RouterBase.tsx
+++ client/src/core/components/RouterBase.tsx
@@ -299,7 +299,7 @@ export default function RouterBase({ children }: { children: React.ReactNode }) 
 								c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
 								t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
 								y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-							})(window, document, "clarity", "script", "${config.microsoftClarityKey}");`,
+							})(window, document, "clarity", "script", "${config.microsoftClarityKey}");`, // eslint-disable-line
 						'text',
 						false,
 						true
