284fe715a503c2ea2543787300b0101caf4105b1
José Guerreiro <jose-guerreiro@live.com.pt>
2022-05-02 17:19:44 +0100

    Better API viewer in <ComponentsViewer/>

--- client/src/core/components/viewer/API.tsx
+++ client/src/core/components/viewer/API.tsx
@@ -7,27 +7,57 @@
 
 import { get } from 'core/api'
 import CodeBlock from 'core/components/CodeBlock'
+import styles from 'core/styles'
 import { Obj } from 'flawk-types'
 import React, { Component } from 'react'
+import Collapsible from '../Collapsible'
 import { Section } from './ComponentsViewer'
 
 export default class API extends Component {
 	state: {
-		api?: Obj
+		api?: { paths: Obj }
 	} = {}
 
 	async componentDidMount() {
 		const res = await get('api')
-		if (res.ok) this.setState({ api: res })
+		if (res.ok) this.setState({ api: res.body })
 	}
 
 	render() {
 		return (
 			<div>
-				<Section title='API' top tags={['<CodeBlock/>']}>
-					{this.state.api && (
-						<CodeBlock lang='json' data={JSON.stringify(this.state.api)} />
-					)}
+				<Section title='API' top>
+					{this.state.api &&
+						this.state.api.paths &&
+						Object.keys(this.state.api.paths).map((p) => {
+							return (
+								<Collapsible
+									key={p}
+									trigger={(isOpen, set) => (
+										<b
+											style={{
+												color: isOpen ? styles.colors.main : undefined,
+											}}
+										>
+											{this.state.api &&
+												Object.keys(
+													this.state.api.paths[p] as Obj
+												)[0].toUpperCase() +
+													' ' +
+													p}
+										</b>
+									)}
+									content={(set) => (
+										<CodeBlock
+											lang='json'
+											data={JSON.stringify(
+												this.state.api && this.state.api.paths[p]
+											)}
+										/>
+									)}
+								></Collapsible>
+							)
+						})}
 				</Section>
 			</div>
 		)
