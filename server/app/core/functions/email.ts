2a53652363ae3c1fcc0060b9c23f80636f4c3cea
José Guerreiro <jose-guerreiro@live.com.pt>
2022-11-06 23:28:23 +0100

    [nodemailer-mjml]: Run mustache after mjml compilation

--- server/app/core/functions/email.ts
+++ server/app/core/functions/email.ts
@@ -35,7 +35,7 @@ function setupNodemailer() {
 		'compile',
 		nodemailerMjmlPlugin({
 			templateFolder: dir,
-			minifyHtmlOutput: true,
+			minifyHtmlOutput: false,
 		})
 	)
 	nodemailerClient.use('compile', htmlToText())
