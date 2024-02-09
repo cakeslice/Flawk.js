0ef023c1cc668d195c0e4068defdccdae3bfff36
José Guerreiro <jose-guerreiro@live.com.pt>
2024-02-09 15:56:18 +0100

    Fix webp format override for vite-imagetools

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,12 +5,12 @@ import macrosPlugin from 'vite-plugin-babel-macros'
 import checker from 'vite-plugin-checker'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-const image = 'fit=inside&webp&quality=100&'
+const image = 'fit=inside&format=webp&quality=100&'
 const iconImage = image + 'w=75&h=75'
 const thumbImage = image + 'w=350&h=350'
 const defaultImage = image + 'w=800&h=800'
 const imageProcessor = {
-	original: new URLSearchParams('fit=inside&webp&quality=100'),
+	original: new URLSearchParams('fit=inside&format=webp&quality=100'),
 	icon: new URLSearchParams(iconImage),
 	thumb: new URLSearchParams(thumbImage),
 	default: new URLSearchParams(defaultImage),
